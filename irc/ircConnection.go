5fd6c131d7c3392a7078b7aba4c901f3ac280640
ceriath <ceriath@ceriath.net>
2017-11-19 21:20:17 +0100

    unique channels on reconnect..

--- irc/ircConnection.go
+++ irc/ircConnection.go
@@ -20,7 +20,7 @@ var ArchiumDataIdentifier = "Message"
 type IrcConnection struct {
 	client                      *network.Client
 	oauth, Username, Host, Port string
-	JoinedChannels              []string
+	JoinedChannels              map[string]struct{}
 	currentReconnectAttempts    int
 	closed                      bool
 	ModOnly                     bool
@@ -46,6 +46,7 @@ func (ircConn *IrcConnection) Init(oauth, nick string) {
 	til.ArchiumPrefix = ArchiumPrefix
 	til.IrcConn = ircConn
 	archiumCore.Register(til)
+	ircConn.JoinedChannels = make(map[string]struct{})
 	ircConn.oauth = oauth
 	ircConn.Username = nick
 	ircConn.privmsgLimiter = new(network.Ratelimiter)
@@ -58,8 +59,9 @@ func (ircConn *IrcConnection) Init(oauth, nick string) {
 	ircConn.joinLimiter.Init("twitchirc-join", 50, 15*time.Second)
 	ircConn.Sendln("PASS " + oauth)
 	ircConn.Sendln("NICK " + nick)
-	ircConn.Sendln("CAP REQ :twitch.tv/tags")
-	ircConn.Sendln("CAP REQ :twitch.tv/commands")
+	ircConn.Sendln("CAP REQ :twitch.tv/tags twitch.tv/commands")
+
+
 	ircConn.currentReconnectAttempts = 0
 	time.Sleep(3 * time.Second)
 	go ircConn.start()
@@ -70,7 +72,7 @@ func (ircConn *IrcConnection) start() {
 		line, err := ircConn.client.Recv()
 		if err != nil {
 			if ircConn.closed {
-				return
+				break
 			}
 			log.E("Error occured", err.Error())
 			ircConn.Reconnect()
@@ -110,17 +112,12 @@ func (ircConn *IrcConnection) BlockingSend(line, channel string) {
 }
 
 func (ircConn *IrcConnection) Join(channel string) {
-	ircConn.JoinedChannels = append(ircConn.JoinedChannels, channel)
+	ircConn.JoinedChannels[channel] = struct{}{}
 	go ircConn.joinInternal("JOIN #" + channel)
 }
 
 func (ircConn *IrcConnection) Leave(channel string) {
-	for i, c := range ircConn.JoinedChannels {
-		if c == channel {
-			ircConn.JoinedChannels = append(ircConn.JoinedChannels[:i], ircConn.JoinedChannels[i+1:]...)
-			break
-		}
-	}
+	delete(ircConn.JoinedChannels, channel)
 	go ircConn.sendlnInternal("PART #" + channel)
 }
 
@@ -155,10 +152,11 @@ func (ircConn *IrcConnection) Reconnect() {
 	}
 	log.I("Trying to recover, attempt:", ircConn.currentReconnectAttempts)
 	time.Sleep(time.Duration(ircConn.currentReconnectAttempts*10) * time.Second)
+	tmpJoinedChannels := ircConn.JoinedChannels
 	ircConn.currentReconnectAttempts++
 	ircConn.Connect(ircConn.Host, ircConn.Port)
 	ircConn.Init(ircConn.oauth, ircConn.Username)
-	for _, channel := range ircConn.JoinedChannels {
-		ircConn.Join(channel)
+	for k, _ := range  tmpJoinedChannels{
+		ircConn.Join(k)
 	}
 }
---
+++ pubsub/pubsub.go
@@ -0,0 +1,137 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"github.com/ceriath/goBlue/archium"
+	"github.com/ceriath/goBlue/log"
+	"github.com/ceriath/goBlue/util"
+	"golang.org/x/net/websocket"
+	"time"
+)
+
+var ArchiumPrefix = "twitch.pubsub."
+var ArchiumDataIdentifier = "Message"
+
+type Pubsub struct {
+	Url string
+	ws  *websocket.Conn
+}
+
+type psListen struct {
+	Type  string `json:"type"`
+	Nonce string `json:"nonce"`
+	Data  struct {
+		Topics    []string `json:"topics"`
+		AuthToken string   `json:"auth_token"`
+	} `json:"data"`
+}
+
+type PsResult struct {
+	Type  string `json:"type"`
+	Nonce string `json:"nonce"`
+	Error string `json:"error"`
+	Data  struct {
+		Topic   string                 `json:"topic"`
+		RawMessage string `json:"message"`
+		Message map[string]interface{} `json:"-"`
+	} `json:"data"`
+}
+
+func (ps *Pubsub) Connect() {
+	ws, err := websocket.Dial(ps.Url, "", "http://localhost/")
+	if err != nil {
+		log.F(err)
+	}
+	ps.ws = ws
+	go func() {
+		for {
+			ps.ping()
+			time.Sleep(3 * time.Minute)
+		}
+	}()
+}
+
+func (ps *Pubsub) Wait() {
+	for {
+		err, msg := ps.recv()
+		if err != nil {
+			log.E(err)
+			if err.Error() == "EOF" {
+				ps.Connect()
+			}
+			continue
+		}
+		go ps.parse(msg)
+	}
+}
+
+func (ps *Pubsub) Listen(topics []string, token string) {
+	ls := new(psListen)
+	ls.Type = "LISTEN"
+	ls.Nonce = util.GetRandomAlphanumericString(10)
+	ls.Data.Topics = topics
+	ls.Data.AuthToken = token
+
+	msg, err := json.Marshal(ls)
+	if err != nil {
+		log.F(err)
+	}
+	log.D("SENT", msg)
+	ps.ws.Write(msg)
+
+}
+
+func (ps *Pubsub) ping() {
+	ping := make(map[string]string)
+	ping["type"] = "PING"
+	data, _ := json.Marshal(ping)
+	ps.ws.Write(data)
+}
+
+func (ps *Pubsub) recv() (error, []byte) {
+	var msg = make([]byte, 2048)
+	var n int
+	n, err := ps.ws.Read(msg)
+	if err != nil {
+		log.E(err)
+		return err, nil
+	}
+	return nil, msg[:n]
+}
+
+func (ps *Pubsub) parse(msg []byte) {
+	psR := new(PsResult)
+	json.Unmarshal(msg, psR)
+	switch psR.Type {
+	case "PONG":
+		return
+	case "RECONNECT":
+		ps.reconnect()
+	case "RESPONSE":
+		log.D(string(msg))
+	default:
+		if err := json.Unmarshal(msg, &psR); err != nil {
+			log.E(err)
+		}
+		if err := json.Unmarshal([]byte(psR.Data.RawMessage), &psR.Data.Message); err != nil {
+			log.E(err)
+		}
+		log.D(psR)
+		ev := archium.CreateEvent()
+		ev.EventType = ArchiumPrefix + psR.Data.Topic
+		ev.EventSource = "Twitch PubSub"
+		ev.Data[ArchiumDataIdentifier] = psR
+		archium.ArchiumCore.FireEvent(*ev)
+
+	}
+
+}
+
+func (ps *Pubsub) Close() {
+	ps.ws.Close()
+}
+
+func (ps *Pubsub) reconnect() {
+	ps.Close()
+	ps.Connect()
+}
